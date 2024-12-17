1eca2a55d755501a4a47f5ecd30c2ded668ea318
It Dterra <it@dterra.eu>
2024-12-17 14:43:10 +0300

    1

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express')
-var bodyParser = require('body-parser')
+const https = require('https')
+const bodyParser = require('body-parser')
 
+const privateKey  = fs.readFileSync('ssl/key.key', 'utf8')
+const certificate = fs.readFileSync('ssl/crt.crt', 'utf8')
+
+const credentials = {key: privateKey, cert: certificate}
 const app = express()
 const port = 3000
+var httpsServer = https.createServer(credentials, app)
 
 const jsonParser = bodyParser.json()
 
-app.post('/log-push', jsonParser, (req, res) => {
+httpsServer.post('/log-push', jsonParser, (req, res) => {
   const payload = req.body
   console.log(payload)
   res.status(200).send('done')
 })
 
-app.listen(port, () => {
+httpsServer.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
